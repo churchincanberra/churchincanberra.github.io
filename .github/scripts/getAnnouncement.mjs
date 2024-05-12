319d55bc289c68d9640c9ea661a1ae64a1ad926b
Kitty Chiu <42864823+KittyChiu@users.noreply.github.com>
2024-05-12 22:53:52 +1000

    feat: get announcement updates from rss

--- .github/scripts/getAnnouncement.mjs
+++ .github/scripts/getAnnouncement.mjs
@@ -1,39 +1,36 @@
-import { getFirstLinkUrl, getPreviousLinksHtml, trimHtmlContent, updatePage } from './utils.mjs';
+import { updatePage } from './utils.mjs';
+import { extractPreviousLinks, extractFirstContentEncoded } from './rssExtractor.mjs';
 import fetch from 'node-fetch';
 import fs from 'fs/promises';
 
-const url = 'https://churchincanberra.us10.list-manage.com/generate-js/?u=12a6ecea8fbc1ad37a233cac1&fid=17501&show=3';
+const URL = 'https://us10.campaign-archive.com/feed?u=12a6ecea8fbc1ad37a233cac1&amp;id=d479a108b4';
+const LIST_SIZE = 3;
 const ANNOUNCEMENT_FILEPATH = './e4b1ffcb4ee645488a4729bd77e0b3d89bc0a70d/announcements.md';
 const ANNOUNCEMENT_TAG = 'ANNOUNCEMENT';
-console.log('Mailchimp Archive URL : ', url);
+console.log('Mailchimp RSS URL : ', URL);
 
 let previousLinksHtml = '';
 
-await fetch(url)
+await fetch(URL)
   .then(response => response.text())
   .then(data => {
     // console.log('data: ', data);
 
-    const latestLink = getFirstLinkUrl(data);
-    // console.log('Email campaign URL: ', latestLink);
+    const firstContentHtml = extractFirstContentEncoded(data);
+    // console.log('First Content: ', firstContentHtml);
     
-    previousLinksHtml = getPreviousLinksHtml(data);
-    // console.log('previousLinksHtml: ', previousLinksHtml);
+    previousLinksHtml = extractPreviousLinks(data, LIST_SIZE);
+    // console.log('Previous Links: ', previousLinksHtml);
 
-    return fetch(latestLink);
-  })
-  .then(response => response.text())
-  .then(async htmlContent => {
-    let newContent;
-    newContent = trimHtmlContent(htmlContent);
+    let newContent = firstContentHtml
     newContent += previousLinksHtml;
 
-    await fs.readFile(ANNOUNCEMENT_FILEPATH, 'utf8')
-      .then(previousContent => {
-        const announcement = updatePage(ANNOUNCEMENT_TAG, previousContent, newContent);
+    fs.readFile(ANNOUNCEMENT_FILEPATH, 'utf8')
+		.then(previousContent => {
+			const announcement = updatePage(ANNOUNCEMENT_TAG, previousContent, newContent);
 
-        return fs.writeFile(ANNOUNCEMENT_FILEPATH, announcement);
-      });
+			return fs.writeFile(ANNOUNCEMENT_FILEPATH, announcement);
+		});
   })
   .catch(error => {
     console.error('Error:', error);
